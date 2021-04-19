97c566dcfdbc2f672a1ff7a19acc7606867928a1
Dustin Micklos <damicklo5@gmail.com>
2021-04-19 16:15:48 -0500

    IndexDB instituted

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,31 +1,34 @@
 const { Pizza } = require('../models');
 
 const pizzaController = {
-  // the functions will go in here as methods
-  // get all pizzas.  We are using the Mongoose .find() method, much like the Sequelize .findAll() method.
+  // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
 
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => {
-        // If no pizza is found, send 404
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
+      .populate({
+        path: 'comments',
+        select: '-__v'
       })
+      .select('-__v')
+      .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
 
@@ -33,10 +36,10 @@ const pizzaController = {
   createPizza({ body }, res) {
     Pizza.create(body)
       .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => res.status(400).json(err));
+      .catch(err => res.json(err));
   },
 
-  // update pizza by id.  If we don't set that third parameter, { new: true }, it will return the original document. By setting the parameter to true, we're instructing Mongoose to return the new version of the document.
+  // update pizza by id
   updatePizza({ params, body }, res) {
     Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
       .then(dbPizzaData => {
@@ -46,21 +49,15 @@ const pizzaController = {
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.status(400).json(err));
+      .catch(err => res.json(err));
   },
 
-  // delete pizza.  We are using the Mongoose .findOneAndDelete() method, which will find the document to be returned and also delete it from the database.
+  // delete pizza
   deletePizza({ params }, res) {
     Pizza.findOneAndDelete({ _id: params.id })
-      .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch(err => res.status(400).json(err));
-  },
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
+  }
 };
 
 module.exports = pizzaController;
